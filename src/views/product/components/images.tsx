c7a3badd6cfcece7b207cda33ce01c524976cd31
Druth <muh.r.kartal@gmail.com>
2024-03-04 00:47:55 +0300

    Product image size

--- src/views/product/components/images.tsx
+++ src/views/product/components/images.tsx
@@ -7,23 +7,21 @@ import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { useRef } from 'react';
-
 export const Images = (props) => {
   const { product } = props;
 
   return (
     <>
-      <Swiper className="flex-[2] [&>.swiper-wrapper]:max-h-full [&>.swiper-wrapper]:h-full [&>.swiper-wrapper]:flex">
+      <Swiper className="flex-[1.8] [&>.swiper-wrapper]:max-h-full [&>.swiper-wrapper]:h-full [&>.swiper-wrapper]:flex">
         {product.images.map((item: ProductItemImagesType) => (
           <SwiperSlide className="w-[auto] flex-shrink-0 block h-full max-h-full">
             <Image
               src={item.image}
               alt={item.alt}
               sizes="(max-width:720px)170px, 230px"
-              width={300}
-              height={400}
-              imageHeight={300}
+              width={400}
+              height={500}
+              imageHeight={400}
               fillWithSize
               showBG
               objectType="object-cover"
